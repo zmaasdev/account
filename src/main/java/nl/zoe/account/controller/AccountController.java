0cb9852eb26fe157f44d1de61158df84dcc1c8f7
Zoe Maas <neo.zoe.maas@gmail.com>
2025-01-16 18:02:24 +0100

    chore: Create account controller

---
+++ src/main/java/nl/zoe/account/controller/AccountController.java
@@ -0,0 +1,24 @@
+package nl.zoe.account.controller;
+
+import nl.zoe.account.dto.AccountDTO;
+import nl.zoe.account.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public AccountDTO create(@RequestBody AccountDTO accountDTO) {
+        return this.accountService.create(accountDTO);
+    }
+}
